a90b245f3f5f87b00e17de223e0a49f05e753393
MohammoD2 <143773181+MohammoD2@users.noreply.github.com>
2024-06-23 08:54:48 +0600

    cukkicutter initial commant

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project will help recomanded the top 3 food item on the swiggy home page',
+    author='Mohammod Ibrahim Hossain',
+    license='',
+)
